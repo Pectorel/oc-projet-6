8fbca5140f32061d80611bc6ba46b13394f317a2
Aurelien <contact@devadra.com>
2023-01-27 13:54:43 +0100

    switch css to sass + installation eslint

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,21 +7,27 @@ function photographerFactory(data) {
     {
         const $article = document.createElement( "article" );
         $article.classList.add("photographer");
+
+        const $img_container= document.createElement("div");
+        $img_container.classList.add("photographer-img-container");
+        $img_container.style.background = "transparent url(\"" + picture + "\") no-repeat center center";
+
         const $img = document.createElement( "img" );
-        $img.classList.add("photographer-img");
-        $img.setAttribute("src", picture);
+        /*$img.classList.add("photographer-img");
+        $img.setAttribute("src", picture);*/
         const $h2 = document.createElement( "h2" );
+        $h2.classList.add("photographer-name")
         $h2.textContent = name;
         const $city_p = document.createElement("p");
         $city_p.classList.add("photographer-city");
         $city_p.textContent = city + ", " + country;
-        const $tagline_p = document.createElement("sp");
+        const $tagline_p = document.createElement("p");
         $tagline_p.classList.add("photographer-tagline");
         $tagline_p.textContent = tagline;
         const $price_p = document.createElement("p");
         $price_p.classList.add("photographer-price");
         $price_p.textContent = price + "€/jour";
-        $article.appendChild($img);
+        $article.appendChild($img_container);
         $article.appendChild($h2);
         $article.appendChild($city_p);
         $article.appendChild($tagline_p);
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,52 +1,44 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        return new Promise((resolve) => {
+async function getPhotographers() {
+  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage
+  // du projet,
+  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  return new Promise((resolve) => {
+    const photographers = [];
+    fetch('./data/photographers.json')
+      .then((res) => {
+        res.json()
+          .then((data) => {
+            const people = data.photographers;
+            // eslint-disable-next-line no-restricted-syntax
+            for (const key in people) {
+              if (people.hasOwnProperty(key)) {
+                const photographer = people[key];
+                photographers.push(photographer);
+              }
+            }
 
-            let photographers = [];
-            fetch("./data/photographers.json")
-                .then(function (res){
-                        res.json()
-                            .then(function (data){
+            resolve(photographers);
+          });
+      });
+  });
+}
 
-                                let people = data.photographers;
-                                for(let key in people) {
+async function displayData(photographers) {
+  const photographersSection = document.querySelector('.photographer_section');
 
-                                    if(people.hasOwnProperty(key)){
+  console.log(photographers);
 
-                                        let photographer = people[key];
-                                        photographers.push(photographer);
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
-                                    }
+async function init() {
+  // Récupère les datas des photographes
+  const photographers = await getPhotographers();
+  displayData(photographers);
+}
 
-                                }
-
-                                resolve(photographers);
-
-                            });
-                    }
-                );
-        });
-
-    }
-
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        console.log(photographers);
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const photographers = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+init();
