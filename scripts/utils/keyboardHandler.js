89dd223de845e01fbd7927d4d3aa5a39ecb07ca2
Aurelien <contact@devadra.com>
2023-02-22 19:33:37 +0100

    Added escape button for keyboard on lightbox

--- scripts/utils/keyboardHandler.js
+++ scripts/utils/keyboardHandler.js
@@ -4,9 +4,13 @@ window.addEventListener("load", () => {
     let $lightbox = document.querySelector("#media-lightbox");
     let $previous_btn = document.querySelector(".lightbox-media-prev");
     let $next_btn = document.querySelector(".lightbox-media-next");
+    let $close_btn = document.querySelector(".close-lightbox");
 
 
     // Listbox
+
+
+
 
     // Keyboard Events
     document.addEventListener('keydown', (event) => {
@@ -18,8 +22,11 @@ window.addEventListener("load", () => {
             if(event.key === "ArrowLeft") {
                 $previous_btn.click();
             }
-            else if(event.key === "ArrowRight"){
+            else if(event.key === "ArrowRight") {
                 $next_btn.click();
+            }
+            else if(event.key === "Escape") {
+                $close_btn.click();
             }
 
         }
