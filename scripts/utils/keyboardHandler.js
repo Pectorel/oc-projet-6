2f52b469806634db5609466cc05e1dbee6b1271a
Aurelien <contact@devadra.com>
2023-02-22 20:10:09 +0100

    Added Listbox Keyboard Events

--- scripts/utils/keyboardHandler.js
+++ scripts/utils/keyboardHandler.js
@@ -8,10 +8,11 @@ window.addEventListener("load", () => {
 
 
     // Listbox
+    let $listbox = document.querySelector("#sort-listbox");
+    let $listbox_options = $listbox.children;
+    console.log($listbox_options);
 
-
-
-
+    
     // Keyboard Events
     document.addEventListener('keydown', (event) => {
 
@@ -31,9 +32,70 @@ window.addEventListener("load", () => {
 
         }
 
+        // Listbox Event
+        if($listbox.classList.contains("active")) {
+
+
+            if (event.key === "ArrowDown") {
+
+                event.preventDefault();
+                changeSelectedOption($listbox_options, "next");
+
+
+            }
+            else if(event.key === "ArrowUp") {
+                event.preventDefault();
+                changeSelectedOption($listbox_options, "prev");
+
+            }
+            else if(event.key === "Enter") {
+                event.preventDefault();
+
+                let $selected = document.querySelector("[data-listbox-option].selected");
+
+                if($selected != null) $selected.click();
+
+            }
+        }
+
     });
 
-    // Listbox Event
-
-
 });
+
+
+function changeSelectedOption($options, type) {
+
+    let $selected = null;
+    let i = 0;
+
+    for(let $elem of $options) {
+
+        if($elem.classList.contains("selected")){
+            if(type === "next") {
+                $selected = $options[i+1];
+            }
+            else {
+                $selected = $options[i-1];
+            }
+
+            $elem.classList.remove("selected");
+        }
+        i++;
+
+    }
+
+    if($selected == null) {
+
+        if(type === "next") {
+            $selected = $options[0];
+        }
+        else {
+            $selected = $options[$options.length - 1];
+        }
+
+    }
+
+    $selected.classList.add("selected");
+    $selected.focus();
+
+}
